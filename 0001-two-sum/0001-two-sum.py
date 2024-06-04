c2156308a22790c1c51ed92d8b27d6959d517ab5
affan002 <107426148+affan002@users.noreply.github.com>
2024-06-04 22:23:27 +0500

    Time: 54 ms (84.79%), Space: 17.7 MB (38.50%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        my_dict = {}
+        for i in range(len(nums)):
+            diff = target - nums[i] 
+            if diff in my_dict.keys(): 
+                return sorted([i, my_dict[diff]])
+            my_dict[nums[i]] = i
+                
+
+
+        
+
+        
