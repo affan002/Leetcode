20f1b353ad7cb6fdb09f6bb2ec509b7177e51a47
affan002 <107426148+affan002@users.noreply.github.com>
2024-06-11 22:35:56 +0500

    Time: 42 ms (22.52%), Space: 17.6 MB (67.58%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -8,16 +8,25 @@
 #     def __init__(self):
 #         self.head = None
 #         self.tail = self.head
+
 class Solution:
+    
+    def helperfunc(self, cur, prev):
+        if cur == null:
+            return prev
+
+        else:
+            temp = cur.next
+            cur.next = prev
+        
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev, cur = None, head
         while cur:
             temp = cur.next
             cur.next = prev
-            prev = cur 
+            prev = cur
             cur = temp
-        
         return prev
-        
-        
+
+
         
