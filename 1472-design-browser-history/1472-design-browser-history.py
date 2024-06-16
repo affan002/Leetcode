032d309393671995e4f77f33d3790bf159d4d715
affan002 <107426148+affan002@users.noreply.github.com>
2024-06-16 16:23:57 +0500

    Time: 159 ms (99.13%), Space: 19 MB (77.36%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,30 +1,74 @@
-class Node:
-    def __init__(self, data, prev=None, next=None):
-        self.data = data
-        self.prev = prev
-        self.next = next
-
+# using an array
 class BrowserHistory:
     def __init__(self, homepage: str):
-        self.cur = Node(homepage)
+        self.i = 0
+        self.len = 1
+        self.hist = [homepage]
+        
 
     def visit(self, url: str) -> None:
-        newNode = Node(data=url, prev=self.cur)
-        self.cur.next = newNode
-        self.cur = newNode
+        if len(self.hist) == self.i + 1:
+            self.hist.append(url)
+        else:
+            self.hist[self.i+1] = url
+
+        self.i+=1
+        self.len = self.i+1
+        
         
 
     def back(self, steps: int) -> str:
-        while self.cur.prev and steps>0:
-            self.cur = self.cur.prev
-            steps-=1
-        return self.cur.data
+        new_pos = self.i - steps
+        if new_pos > 0:
+            self.i = new_pos
+        else:
+            self.i = 0
+        
+        return self.hist[self.i]
+        
 
     def forward(self, steps: int) -> str:
-        while self.cur.next and steps>0:
-            self.cur = self.cur.next
-            steps-=1
-        return self.cur.data
+        new_pos = self.i + steps
+        if new_pos > self.len - 1:
+            self.i = self.len - 1
+        else:
+            self.i = new_pos
+
+        return self.hist[self.i]
+
+        
+        
+
+
+
+# using doublylinkedlist
+# class Node:
+#     def __init__(self, data, prev=None, next=None):
+#         self.data = data
+#         self.prev = prev
+#         self.next = next
+
+# class BrowserHistory:
+#     def __init__(self, homepage: str):
+#         self.cur = Node(homepage)
+
+#     def visit(self, url: str) -> None:
+#         newNode = Node(data=url, prev=self.cur)
+#         self.cur.next = newNode
+#         self.cur = newNode
+        
+
+#     def back(self, steps: int) -> str:
+#         while self.cur.prev and steps>0:
+#             self.cur = self.cur.prev
+#             steps-=1
+#         return self.cur.data
+
+#     def forward(self, steps: int) -> str:
+#         while self.cur.next and steps>0:
+#             self.cur = self.cur.next
+#             steps-=1
+#         return self.cur.data
         
         
 
