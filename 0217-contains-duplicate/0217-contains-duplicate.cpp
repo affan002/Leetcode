7ad7231a5e871330e0a5a147250f84261f4e2a2f
affan002 <ma08910@st.habib.edu.pk>
2025-05-11 22:45:43 +0500

    Time: 43 ms (45.04%), Space: 90.7 MB (28.75%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        std::sort(nums.begin(), nums.end());
-        for (int i; i<nums.size()-1; i++) {
-            if (nums[i] == nums[i+1]) {
+        std::unordered_map<int, int> countMap;
+
+        for (int& num: nums){
+            if (countMap.count(num) == 0) 
+                countMap[num]=1;
+            else 
                 return true;
-            }
         }
+
         return false;
     }
 };
