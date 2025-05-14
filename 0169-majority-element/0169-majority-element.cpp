48a6c4868fe072d9e52137c7958c013c1c8d31ee
affan002 <ma08910@st.habib.edu.pk>
2025-05-14 22:41:48 +0500

    Time: 0 ms (100%), Space: 28 MB (97.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int maxCount=0;
-        int res;
-        unordered_map<int, int> hashMap;
-        for (int num : nums) {
-            if (hashMap.find(num) != hashMap.end()) {
-                hashMap[num]++;
-            } else {
-                hashMap[num] = 1;
-            }
+        int res = nums[0];
+        int count = 1;
 
-            if (hashMap[num] > maxCount) {
-                    maxCount = hashMap[num];
-                    res = num;
+        for (int i=1; i<nums.size(); i++) {
+            if (nums[i] == res) {
+                count++;
+
+            } else if (nums[i] != res) {
+                if (count == 0) {
+                    res = nums[i];
+                    count++;
+                    continue;
+                }
+
+                count--;
             }
         }
         return res;
