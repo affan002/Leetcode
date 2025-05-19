1018e48e7573fcf20147e8214ddc6aedfcebff6a
affan002 <ma08910@st.habib.edu.pk>
2025-05-19 16:21:19 +0500

    Time: 2 ms (58.94%), Space: 19.9 MB (11.27%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,59 +1,28 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> count;
-        for (int num : nums) {
-            count[num]++;
+        const int n = nums.size();
+        vector<vector<int>> count_arr(n+1);
+        unordered_map<int,int> count_map;
+
+        for (int num: nums) {
+            count_map[num]++;
         }
 
-        vector<pair<int,int>> count_pairs;
-        for (auto& c : count) {
-            count_pairs.push_back({c.second, c.first});
+        for (auto& c: count_map) {
+            count_arr[c.second].push_back(c.first);
         }
-
-        mergeSort(0, count_pairs.size()-1, count_pairs);
-
-        vector<int> out;
-        for (int i=0; i<k; i++) {
-            out.push_back(count_pairs[i].second);
-        }
-
-        return out;
-    }
-
-private: 
-    void mergeSort(int left, int right, vector<pair<int,int>>& array) {
-        //base case
-        if (left>=right) return;
-
-        // calculate mid
-        int mid = (left + right) / 2;
-        //recursive calls 
-        mergeSort(left, mid, array);
-        mergeSort(mid+1, right, array);
-        //merge
-        merge(left, right, mid, array);
-    }
-
-    void merge(int left, int right, int mid, vector<pair<int,int>>& array) {
-        vector<pair<int,int>> temp; // temporary array
-        int i=left, j=mid+1;
-
-        while (i<= mid && j<= right) {
-            if(array[i].first>array[j].first) {
-                temp.push_back(array[i]);
-                i++;
-            } else {
-                temp.push_back(array[j]);
-                j++;
+        vector<int> output;
+        int limit_k = 0;
+        for (int i=n; i>=0 && limit_k<k; i--) {
+            for (int num:count_arr[i]) {
+                output.push_back(num);
+                limit_k++;
+                if (limit_k == k) break;
             }
         }
-
-        while (i<=mid) temp.push_back(array[i++]);
-        while (j<=right) temp.push_back(array[j++]);
-
-        for (int k=left; k<=right; k++) {
-            array[k] = temp[k-left];
-        }
+        return output;
     }
+
+
 };
