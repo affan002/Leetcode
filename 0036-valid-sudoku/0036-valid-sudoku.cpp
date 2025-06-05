a91208c5826750bf70f3cade8c0f41a78b80734e
affan002 <ma08910@st.habib.edu.pk>
2025-06-05 23:37:33 +0500

    Time: 7 ms (36.85%), Space: 26.2 MB (5.4%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -4,25 +4,23 @@ public:
 
     bool isValidSudoku(vector<vector<char>>& board) {
         
-        unordered_map<int, unordered_set<char>> rowMap, colMap;
-        map<pair<int,int>, unordered_set<char>> gridMap;
+        unordered_map<int, unordered_set<int>> rowMap, colMap;
+        map<pair<int,int>, unordered_set<int>> gridMap;
 
-        for (int r=0; r<9; r++) {
-            for(int c=0; c<9; c++) {
-                if (board[r][c] == '.') continue;
-
-                pair<int,int> currPair = {r/3,c/3};
-
-                if (rowMap[r].count(board[r][c]) || 
-                colMap[c].count(board[r][c]) ||
-                gridMap[currPair].count(board[r][c])) return false;
-
-
-                rowMap[r].insert(board[r][c]);
-                colMap[c].insert(board[r][c]);
-                gridMap[currPair].insert(board[r][c]);
+        for (int row= 0; row<9; row++) {
+            for (int col=0; col<9; col++) {
+                pair<int,int> currCell = {row/3, col/3};
+                int currElement = board[row][col];
+                if (currElement == '.') continue;
+                else if (rowMap[row].count(currElement)==1 || colMap[col].count(currElement)==1 || gridMap[currCell].count(currElement)==1) {
+                    return false;
+                }
+                rowMap[row].insert(currElement);
+                colMap[col].insert(currElement);
+                gridMap[currCell].insert(currElement);
             }
         }
+
         return true;
     }
 };
