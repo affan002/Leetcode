e636a646089b5b80b42bdbc70c58955ab7b909f0
affan002 <ma08910@st.habib.edu.pk>
2025-06-15 19:27:35 +0500

    Time: 0 ms (100%), Space: 55 MB (45.73%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        unordered_map<int,int> hashSet;
-        for (int num: nums) {
-            hashSet[num] = 1;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 0) {
+                nums[i] = 0;
+            }
         }
-        
-        for (int i=1; i<=nums.size(); i++) {
-            if (hashSet[i]<1) {
+
+        for (int i = 0; i < n; i++) {
+            int val = abs(nums[i]);
+            if (val >= 1 && val <= n) {
+                if (nums[val - 1] > 0) {
+                    nums[val - 1] *= -1;
+                } else if (nums[val - 1] == 0) {
+                    nums[val - 1] = -1 * (n + 1);
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (nums[i - 1] >= 0) {
                 return i;
             }
         }
-    return nums.size()+1;
+
+        return n + 1;
     }
 };
