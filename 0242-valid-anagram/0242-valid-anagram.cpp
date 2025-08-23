24fad44af16a297e77c3195fbd6a43ff8b0c44db
affan002 <ma08910@st.habib.edu.pk>
2025-08-23 11:29:24 +0500

    Time: 0 ms (100%), Space: 9.9 MB (11.64%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if (s == t)
-            return true; 
-        return false;
+        if (s.size() != t.size()) return false;
+
+        unordered_map<char, int> count_s;
+        unordered_map<char, int> count_t;
+        for (int i=0; i<s.size(); i++) {
+            count_s[s[i]]++;
+            count_t[t[i]]++;
+        }
+
+        return (count_s==count_t);
     }
 };
