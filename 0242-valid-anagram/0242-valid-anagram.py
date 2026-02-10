e838c1778111b7c8082370fcc1f239bce8de6875
Muhammad Affan <ma08910@st.habib.edu.pk>
2026-02-10 12:00:02 +0500

    Time: 11 ms (75.65%), Space: 19.7 MB (16.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t): return False
+
+        map_s, map_t = {}, {}
+
+        for i in range(len(s)):
+            map_s[s[i]] = map_s.get(s[i], 0) + 1
+            map_t[t[i]] = map_t.get(t[i], 0) + 1
+
+        return map_s==map_t
